3fca72ae1f7471944a6bc6e132ab2c58a6e8348b
Brandon <bmiller3822@gmail.com>
2020-01-31 13:23:06 -0500

    Completed number 2.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 console.log('Add validation!');
  
     let fields = document.querySelectorAll(".field")
+    
   
     let parkingForm = document.querySelector("#parking-form")
     parkingForm.addEventListener("submit", function(e){
@@ -9,14 +10,130 @@ console.log('Add validation!');
         console.log(input);
         if (input.value === ""){
             console.log("Nothing here!")
+            if (input.parentElement.classList.contains("input-invalid")){
+            } else {
             input.parentElement.classList.add("input-invalid")
+            let required = document.createElement("p")
+            console.log(required);
+            required.innerText = "*Required"
+            required.classList.add("required")
+            input.parentElement.appendChild(required)
+        }
         } else {
             console.log("Stuff in there")
             input.parentElement.classList.add("input-valid")
+            let required = input.parentElement.querySelector("p")
+            if (required){
+                input.parentElement.removeChild(required);
+            }
         }
     }})
 
+    //Second event listener if needed
+    // parkingForm.addEventListener("submit", function(e){
+    //     e.preventDefault();
 
+    // });
+
+    // if (node.parentNode) {
+    //     // remove a node from the tree, unless 
+    //     // it's not in the tree already
+    //     node.parentNode.removeChild(node);
+    //   }
+
+//failed removal attemps
+    // let reqP = node.parentNode;
+    //         if (reqP.input.parentElement){
+    //             reqP.input.parentElement.removeChild(reqP);
+    //         }
+
+
+    // This worked to remove required from Name!!!
+    // let fields = document.querySelectorAll(".field")
+    
+  
+    // let parkingForm = document.querySelector("#parking-form")
+    // parkingForm.addEventListener("submit", function(e){
+    //     e.preventDefault();
+    //     for (let input of fields){
+    //     console.log(input);
+    //     if (input.value === ""){
+    //         console.log("Nothing here!")
+    //         input.parentElement.classList.add("input-invalid")
+    //         let required = document.createElement("p")
+    //         console.log(required);
+    //         required.innerText = "*Required"
+    //         required.classList.add("required")
+    //         input.parentElement.appendChild(required)
+    //     } else {
+    //         console.log("Stuff in there")
+    //         input.parentElement.classList.add("input-valid")
+    //         let required = input.parentElement.querySelector("p")
+    //         if (required){
+    //             input.parentElement.removeChild(required);
+    //         }
+    //     }
+    // }})
+
+    //Old version:
+    // let parkingForm = document.querySelector("#parking-form")
+    // parkingForm.addEventListener("submit", function(e){
+    //     e.preventDefault();
+    //     for (let input of fields){
+    //     console.log(input);
+    //     if (input.value === ""){
+    //         console.log("Nothing here!")
+    //         input.parentElement.classList.add("input-invalid")
+    //         let required = document.createElement("p")
+    //         console.log(required);
+    //         required.innerText = "*Required"
+    //         required.classList.add("required")
+    //         input.parentElement.appendChild(required)
+    //     } else {
+    //         console.log("Stuff in there")
+    //         input.parentElement.classList.add("input-valid")
+    //         let required = document.querySelector("p")
+    //         if (document.querySelector(required).input.parentElement){
+    //             input.parentElement.removeChild(required);
+    //         }
+    //     }
+    // }})
+
+
+
+    // Need to make cars red
+    // Need to divide by the 3 car divs
+    // Need to create a div and put the "required message" in there if it's incorrect.  Don't repeat.  Maybe remove the div on click or something. 
+    // Step 3: 
+    // Name must be a String
+    // Car year must be a year
+    // make and model as dropdowns?
+    // Date parking is a date of course 
+    // Number of days as a string/number?
+    // Credit card?
+    // CVV must be 3 numbers 
+    // Expiration as month/year
+    // Jebus, 4-6 I have no idea on. 
+
+
+
+
+    //Woop this works!  Now I'm going to try and break it :)
+    // let fields = document.querySelectorAll(".field")
+  
+    // let parkingForm = document.querySelector("#parking-form")
+    // parkingForm.addEventListener("submit", function(e){
+    //     e.preventDefault();
+    //     for (let input of fields){
+    //     console.log(input);
+    //     if (input.value === ""){
+    //         console.log("Nothing here!")
+    //         input.parentElement.classList.add("input-invalid")
+    //     } else {
+    //         console.log("Stuff in there")
+    //         input.parentElement.classList.add("input-valid")
+    //     }
+    // }})
 
 
     // .field
