c9a33b5af795fca8a0541a23bbc254195a0f3f3f
Brandon <bmiller3822@gmail.com>
2020-01-31 18:15:52 -0500

    Finally got the ground work for number 3 to work.

--- script.js
+++ script.js
@@ -1,37 +1,156 @@
 console.log('Add validation!');
- 
-    let fields = document.querySelectorAll(".field")
-    var letters = /^[A-Za-z]+$/;
-    var lettersAndNumbers = /^[0-9a-zA-Z]+$/;
-    
-  
-    let parkingForm = document.querySelector("#parking-form")
-    parkingForm.addEventListener("submit", function(e){
-        e.preventDefault()
-        for (let input of fields){
+
+let fields = document.querySelectorAll(".field")
+let nameInput = document.querySelector("#name-input");
+let carYear = document.querySelector("#car-year");
+let carMake = document.querySelector("#car-make");
+let carModel = document.querySelector("#car-model");
+let startDate = document.querySelector("#start-date");
+let days = document.querySelector("#days");
+let creditCard = document.querySelector("#credit-card");
+let cvv = document.querySelector("#cvv");
+let expiration = document.querySelector("#expiration");
+let letters = /^[A-z]+$/; 
+let lettersAndNumbers = /^[0-9a-zA-Z]+$/ ;
+
+let parkingForm = document.querySelector("#parking-form")
+parkingForm.addEventListener("submit", function (e) {
+    e.preventDefault()
+    for (let input of fields) {
         console.log(input)
-        if (input.value === ""){
+        if (input.value === "") {
             console.log("Nothing here!")
-            if (input.parentElement.classList.contains("input-invalid")){
+            if (input.parentElement.classList.contains("randomOne")) {  //"input-invalid"  and classList  if (input.parentElement.classList.contains(p))
             } else {
-            input.parentElement.classList.remove("input-valid")
-            input.parentElement.classList.add("input-invalid")
+                input.parentElement.classList.add("randomOne")
+                input.parentElement.classList.remove("randomTwo")
+                input.parentElement.classList.remove("input-valid")
+                input.parentElement.classList.add("input-invalid")
+                let noText = input.parentElement.querySelector("p")
+                if (noText) {
+                    input.parentElement.removeChild(noText)
+                }
+                let required = document.createElement("p")
+                console.log(required)
+                required.innerText = "*Required"
+                required.classList.add("required")
+                input.parentElement.appendChild(required)
+            }
+        } else if (!letters.test(nameInput.value)){
+            if (nameInput.parentElement.classList.contains("randomTwo")){ //"input-invalid"
+            } else {
+            nameInput.parentElement.classList.add("randomTwo")
+            nameInput.parentElement.classList.remove("randomOne")
+            nameInput.parentElement.classList.remove("input-valid")
+            nameInput.parentElement.classList.add("input-invalid")
+            let wrongText = nameInput.parentElement.querySelector("p")
+            if (wrongText) {
+                nameInput.parentElement.removeChild(wrongText)
+            }
             let required = document.createElement("p")
             console.log(required)
-            required.innerText = "*Required"
+            required.innerText = "*Please enter Name using [A-z]"
             required.classList.add("required")
-            input.parentElement.appendChild(required)
-        }
+            nameInput.parentElement.appendChild(required)
+            }
+        // } else if (x>0){}
+          
         } else {
-            console.log("Stuff in there")
+            console.log("Stuff in here!")
             input.parentElement.classList.remove("input-invalid")
+            input.parentElement.classList.remove("randomOne")
+            input.parentElement.classList.remove("randomTwo")
             input.parentElement.classList.add("input-valid")
-            let requiredTwo = input.parentElement.querySelector("p")
-            if (requiredTwo){
-                input.parentElement.removeChild(requiredTwo)
+            let required = input.parentElement.querySelector("p")
+            if (required) {
+                input.parentElement.removeChild(required)
+            }
+            let requiredTwo = nameInput.parentElement.querySelector("p")
+            if (requiredTwo) {
+                nameInput.parentElement.removeChild(requiredTwo)
             }
         }
-    }})
+    }
+})
+
+
+
+//I'm about to break this: 
+// let fields = document.querySelectorAll(".field")
+// let nameInput = document.querySelector("#name-input");
+// let carYear = document.querySelector("#car-year");
+// let carMake = document.querySelector("#car-make");
+// let carModel = document.querySelector("#car-model");
+// let startDate = document.querySelector("#start-date");
+// let days = document.querySelector("#days");
+// let creditCard = document.querySelector("#credit-card");
+// let cvv = document.querySelector("#cvv");
+// let expiration = document.querySelector("#expiration");
+// let letters = /^[A-z]+$/; 
+// let lettersAndNumbers = /^[0-9a-zA-Z]+$/ ;
+
+// let parkingForm = document.querySelector("#parking-form")
+// parkingForm.addEventListener("submit", function (e) {
+//     e.preventDefault()
+//     for (let input of fields) {
+//         console.log(input)
+//         if (input.value === "") {
+//             console.log("Nothing here!")
+//             if (input.parentElement.contains(p)) {  //"input-invalid"  and classList  if (input.parentElement.classList.contains(p))
+//             } else {
+//                 input.parentElement.classList.remove("input-valid")
+//                 input.parentElement.classList.add("input-invalid")
+//                 let required = document.createElement("p")
+//                 console.log(required)
+//                 required.innerText = "*Required"
+//                 required.classList.add("required")
+//                 input.parentElement.appendChild(required)
+//             }
+//         } else if (!letters.test(nameInput.value)){
+//             if (nameInput.parentElement.contains(p)){ //"input-invalid"
+//             } else {
+//             nameInput.parentElement.classList.remove("input-valid")
+//             nameInput.parentElement.classList.add("input-invalid")
+//             let required = document.createElement("p")
+//             console.log(required)
+//             required.innerText = "*Please enter Name using [A-z]"
+//             required.classList.add("required")
+//             nameInput.parentElement.appendChild(required)
+//             }
+//         // } else if (x>0){}
+          
+//         } else {
+//             console.log("Stuff in here!")
+//             input.parentElement.classList.remove("input-invalid")
+//             input.parentElement.classList.add("input-valid")
+//             let requiredTwo = input.parentElement.querySelector("p")
+//             if (requiredTwo) {
+//                 input.parentElement.removeChild(requiredTwo)
+//             }
+//         }
+//     }
+// })
+
+
+
+
+
+//Noting this for when I inevitably break it:
+// let nameInput = document.querySelector("#name-input");
+// if(!letters.test(nameInput.value)){
+//     nameInput.parentElement.classList.remove("input-valid")
+//     nameInput.parentElement.classList.add("input-invalid")
+//     let required = document.createElement("p")
+//     console.log(required)
+//     required.innerText = "*Please enter Name using [A-z]"
+//     required.classList.add("required")
+//     nameInput.parentElement.appendChild(required)
+// }
+
+// Failed attempt at reg ex
+// if (document.querySelector("#name-input").value.match(letters)){
+//     console.log("TADA!");
+// }
 
 //Need to do the 3 car fields as well.  
 // Name = Text
@@ -46,6 +165,34 @@ console.log('Add validation!');
 
 
 
+//THIS WORKS!  END OF STEP 2. LET'S BREAK IT AGAIN FOR STEP 3 :) 
+// let parkingForm = document.querySelector("#parking-form")
+//     parkingForm.addEventListener("submit", function(e){
+//         e.preventDefault()
+//         for (let input of fields){
+//         console.log(input)
+//         if (input.value === ""){
+//             console.log("Nothing here!")
+//             if (input.parentElement.classList.contains("input-invalid")){
+//             } else {
+//             input.parentElement.classList.remove("input-valid")
+//             input.parentElement.classList.add("input-invalid")
+//             let required = document.createElement("p")
+//             console.log(required)
+//             required.innerText = "*Required"
+//             required.classList.add("required")
+//             input.parentElement.appendChild(required)
+//         }
+//         } else {
+//             console.log("Stuff in here!")
+//             input.parentElement.classList.remove("input-invalid")
+//             input.parentElement.classList.add("input-valid")
+//             let requiredTwo = input.parentElement.querySelector("p")
+//             if (requiredTwo){
+//                 input.parentElement.removeChild(requiredTwo)
+//             }
+//         }
+//     }})
 
 
 // Function to check letters and numbers
@@ -88,7 +235,7 @@ console.log('Add validation!');
     //if there's nothing in the field that we're looping through, add the "input-invalid" class
     //the else statement is saying if there's ANYTHING in the field, add the "input-valid" class
 
-  
+
     //Note:
     // if (node.parentNode) {
     //     // remove a node from the tree, unless 
@@ -105,8 +252,8 @@ console.log('Add validation!');
 
     // This worked to remove required from Name!!!
     // let fields = document.querySelectorAll(".field")
-    
-  
+
+
     // let parkingForm = document.querySelector("#parking-form")
     // parkingForm.addEventListener("submit", function(e){
     //     e.preventDefault();
@@ -175,7 +322,7 @@ console.log('Add validation!');
 
     //Woop this works!  Now I'm going to try and break it :)
     // let fields = document.querySelectorAll(".field")
-  
+
     // let parkingForm = document.querySelector("#parking-form")
     // parkingForm.addEventListener("submit", function(e){
     //     e.preventDefault();
